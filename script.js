3fdbcb7e1e1bb7fac0442abb27714badc67b5651
MirlaneS <mirlanef35@gmail.com>
2022-09-20 13:52:07 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Olá mundo!");
+/**********************
+ * JavaScript do site *
+ **********************/
+
+/* REMOVER - Este código serve apenas para saber se o JavaScript está funcionando. */
+console.log('Mulheres.Tech');
